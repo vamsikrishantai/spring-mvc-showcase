a22ab6442a7988685639e1a0ee15bb3625a3dfb1
vamsi <vamsi.k6875@gmail.com>
2015-06-30 22:36:04 +0530

    sapce

--- src/main/java/org/springframework/mvc/extensions/ajax/AjaxUtils.java
+++ src/main/java/org/springframework/mvc/extensions/ajax/AjaxUtils.java
@@ -6,6 +6,13 @@ public class AjaxUtils {
 
 	public static boolean isAjaxRequest(WebRequest webRequest) {
 		String requestedWith = webRequest.getHeader("X-Requested-With");
+		
+
+
+
+
+
+		
 		return requestedWith != null ? "XMLHttpRequest".equals(requestedWith) : false;
 	}
 
